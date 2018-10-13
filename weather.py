6cad590017817d477bc8f3fb2f4873ba45b1d3c5
Your name <you@example.com>
2018-10-13 21:40:16 +0200

    making app portable to linux because of paths

--- weather.py
+++ weather.py
@@ -5,27 +5,31 @@ from datetime import datetime
 from tools import (
     load_json_file, save_json_file, current_time, load_json_weather
     )
+from pathlib import Path
 
-Cracow = {
-    "latitude": 50.0646,
-    "longitude": 19.9449
-}
+
+secretkeyPath = Path('data/secret_key.json')
+lastdataPath = Path('data/last_data.json')
+cities = Path('data/cities.json')
+
+citiDict = load_json_file(cities)
+city = citiDict['Lodz']
 
 try:
-    with open("secret_key.json", 'r') as key:
+    with open(secretkeyPath, 'r') as key:
         secret_key = json.loads(key.read())['secret']
 except FileNotFoundError:
     secret_key = input("Please enter you secret key: ")
     new_json_file = {
         "secret": secret_key
     }
-    with open("secret_key.json", 'w') as file:
+    with open(secretkeyPath, 'w') as file:
         file.write(json.dumps(new_json_file, indent=2))
 
 weather_api_url = "https://api.darksky.net/forecast/{}/{},{}?{}".format(
     secret_key,
-    Cracow['latitude'],
-    Cracow['longitude'],
+    city['latitude'],
+    city['longitude'],
     'units=si'
 )
 
@@ -38,7 +42,8 @@ time_of_downloads = datetime.fromtimestamp(
     ).strftime("%A, %B %d, %Y %H:%M:%S")
 
 print(
-    "Actual teperature in Cracow is {} Celcius degrees.".format(
+    "Actual teperature in {} is {} Celcius degrees.".format(
+        city['name'],
         round(float(current_data['temperature']))
     )
 )
@@ -48,19 +53,20 @@ print('Actualization from {}'.format(
     )
 )
 
-save_json_file('last_data.json', data)
+save_json_file(lastdataPath, data)
 
 while (True):
-    last_data = load_json_file('last_data.json')
+    last_data = load_json_file(lastdataPath)
     time_of_download = int(
         last_data['currently']['time']
     )
     if current_time() >= time_of_download+60:
         data = load_json_weather(weather_api_url)
-        save_json_file('last_data.json', data)
+        save_json_file(lastdataPath, data)
 
         print(
-           "Actual teperature in Cracow is {} Celcius degrees.".format(
+           "Actual teperature in {} is {} Celcius degrees.".format(
+                city['name'],
                 round(float(data['currently']['temperature']))
             )
         )
