301df94b61c90a317fcb4956c1fba6a2137f6024
thinkofher <piaskupiasku@gmail.com>
2018-10-14 20:25:30 +0200

    LCD implementation for main loop

--- weather.py
+++ weather.py
@@ -7,14 +7,14 @@ from tools import (
     load_json_weather, load_secret_key,
     current_time
     )
-
+from lcd import lcd
 
 secretkeyPath = Path('data/secret_key.json')
 lastdataPath = Path('data/last_data.json')
 cities = Path('data/cities.json')
 
 citiDict = load_json_file(cities)
-city = citiDict['Lodz']
+city = citiDict['Cracow']
 
 secret_key = load_secret_key(secretkeyPath)
 weather_api_url = "https://api.darksky.net/forecast/{}/{},{}?{}".format(
@@ -42,11 +42,41 @@ print('Actualization from {}.'.format(
 
 save_json_file(lastdataPath, data)
 
+old_message_1 = '{} {}\n'.format(
+    city['name'],
+    round(float(data['currently']['temperature']))
+)
+old_message_2 = '{}'.format(
+    datetime.fromtimestamp(
+        current_time()
+    ).strftime("%H:%M:%S")
+)
+
 while (True):
     last_data = load_json_file(lastdataPath)
     time_of_download = int(
         last_data['currently']['time']
     )
+    
+    message_1 = '{} {}*C\n'.format(
+        city['name'],
+        round(float(last_data['currently']['temperature']))
+    )
+    message_2 = '{}'.format(
+        datetime.fromtimestamp(
+            current_time()
+        ).strftime("%H:%M:%S")
+    )
+    
+    if (
+        (old_message_1 != message_1) or
+        (old_message_2 != message_2)
+    ):
+        lcd.clear()
+        lcd.message(message_1+message_2)
+        old_message_1 = message_1
+        old_message_2 = message_2
+    
     if current_time() >= time_of_download+60:
         data = load_json_weather(weather_api_url)
         save_json_file(lastdataPath, data)
