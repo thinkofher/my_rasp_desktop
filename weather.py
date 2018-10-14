62fcf52814fd9a42106c4a7dc0949bf38c0c606c
thinkofher <piaskupiasku@gmail.com>
2018-10-15 00:45:05 +0200

    Added offline mode

--- weather.py
+++ weather.py
@@ -8,6 +8,7 @@ from tools import (
     current_time
     )
 from lcd import lcd
+from urllib.error import URLError
 
 secretkeyPath = Path('data/secret_key.json')
 lastdataPath = Path('data/last_data.json')
@@ -24,7 +25,14 @@ weather_api_url = "https://api.darksky.net/forecast/{}/{},{}?{}".format(
     'units=si'
 )
 
-data = load_json_weather(weather_api_url)
+offline = False
+try:
+    # if online download newest data from dark sky api
+    data = load_json_weather(weather_api_url)
+except URLError:
+    # if offline just load last avalible data
+    data = load_json_file(lastdataPath)
+    offline = True
 
 print(
     "Actual teperature in {} is {} Celcius degrees.".format(
@@ -53,21 +61,36 @@ old_message_2 = '{}'.format(
 )
 
 while (True):
+
     last_data = load_json_file(lastdataPath)
     time_of_download = int(
         last_data['currently']['time']
     )
     
-    message_1 = '{} {}*C\n'.format(
+    message_1 = '{} {}{}C\n'.format(
         city['name'],
-        round(float(last_data['currently']['temperature']))
-    )
-    message_2 = '{}'.format(
-        datetime.fromtimestamp(
-            current_time()
-        ).strftime("%H:%M:%S")
+        round(float(last_data['currently']['temperature'])),
+        chr(223)
     )
     
+    # view special OFFLINE message on lcd
+    # if offline is True
+    if not offline:
+        message_2 = '{}'.format(
+            datetime.fromtimestamp(
+                current_time()
+            ).strftime("%H:%M:%S")
+        )
+    else:
+        message_2 = '{} OFFLINE'.format(
+            datetime.fromtimestamp(
+                current_time()
+            ).strftime("%H:%M:%S")
+        )
+        
+    
+    # if something changed in messages
+    # update lcd view
     if (
         (old_message_1 != message_1) or
         (old_message_2 != message_2)
@@ -77,8 +100,22 @@ while (True):
         old_message_1 = message_1
         old_message_2 = message_2
     
-    if current_time() >= time_of_download+(5*60):
-        data = load_json_weather(weather_api_url)
+    # if machine is offline, program should
+    # try to check weather more often
+    if offline:
+        wait_time = 15
+    else:
+        wait_time = 5*60
+    
+    if current_time() >= time_of_download+(wait_time):
+        try:
+            # if online download newest data from dark sky api
+            data = load_json_weather(weather_api_url)
+            offline = False
+        except URLError:
+            # if offline just load last avalible data
+            data = load_json_file(lastdataPath)
+            offline = True
         save_json_file(lastdataPath, data)
 
         print(
